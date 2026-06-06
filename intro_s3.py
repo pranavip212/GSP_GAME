c1c80b3c4fb24d8a6b73b348e8001408b5972024
chelseav87 <chelseav87@kprschools.ca>
2026-06-06 04:14:11 -0400

    essentially finished intro_s3 and transition_s3; any remaining changes are in doc

--- intro_s3.py
+++ intro_s3.py
@@ -9,12 +9,12 @@ def play_intro_s3(screen, clock):
     font = pygame.freetype.SysFont("consolas", 28, bold=True)
 
     dialogue_lines = [
-        [("Curiously, you both wander the halls.", WHITE)],
+        [("Curious, you both wander the halls.", WHITE)],
         [("Suddenly, the lights go out...", WHITE)],
         [("You feel the ground move...", WHITE)],
         [("The hallways twist and shift into a dark maze.", WHITE)],
         [("Mila abruptly grabs your wrist:", WHITE)],
-        [("We have to go. Now.", (50, 120, 255))], # change colour to purple once constant is made
+        [("We have to go. Now.", (50, 120, 255))], # change colour to purple once constant is made; maybe indicate speaker (?)
         [("You have to make a quick decision.", WHITE)],
         [("Do you trust Mila?", RED)]]
 
@@ -68,22 +68,30 @@ def play_intro_s3(screen, clock):
 
         if current_line >= 4:
             screen.blit(maze, (0, 0))
-            screen.blit(pygame.transform.scale(mila_normal_dark, (250, 640)), (485, 90))
+            screen.blit(pygame.transform.scale(pygame.transform.flip(mila_normal_dark, True, False), (250, 640)), (485, 90))
 
         if current_line >= 5:
-            screen.blit(pygame.transform.scale(mila_pensive_dark, (250, 640)), (485, 90))
+            screen.blit(pygame.transform.scale(pygame.transform.flip(mila_pensive_dark, True, False), (250, 640)), (485, 90))
 
         if current_line >= 6:
             screen.blit(maze, (0, 0))
-            screen.blit(pygame.transform.scale(mila_normal_dark, (195, 520)), (520, 120))
+            screen.blit(pygame.transform.scale(pygame.transform.flip(mila_normal_dark, True, False), (195, 520)), (520, 120))
 
         if show_choice:
             pygame.draw.rect(screen, BLACK, follow_button)
             pygame.draw.rect(screen, BLACK, run_button)
             pygame.draw.rect(screen, (180, 30, 30), follow_button, 3)
             pygame.draw.rect(screen, (180, 30, 30), run_button, 3)
-            font.render_to(screen, (61, 330), "Follow her.", WHITE)
-            font.render_to(screen, (61, 400), "Run away!", WHITE)
+
+            if follow_button.collidepoint(pygame.mouse.get_pos()):
+                font.render_to(screen, (61, 330), "Follow her.", RED)
+            else:
+                font.render_to(screen, (61, 330), "Follow her.", WHITE)
+
+            if run_button.collidepoint(pygame.mouse.get_pos()):
+                font.render_to(screen, (61, 400), "Run away!", RED)
+            else:
+                font.render_to(screen, (61, 400), "Run away!", WHITE)
 
         dialogue_box.update()
         dialogue_box.draw(screen)
--- transition_s3.py
+++ transition_s3.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from game_states import GameState
+# import fight piece here
 from ui import DialogueBox
 from images import *
 
@@ -8,20 +8,21 @@ from images import *
 def play_transition_s3(screen, clock, condition):
     font = pygame.freetype.SysFont("consolas", 28, bold=True)
 
+    dialogue_lines = []
+
     if condition == "follow_mila":
         dialogue_lines = [
-            [("Conflicted, you decide to follow Mila.", WHITE)],
+            [("Conflicted, you agree to follow Mila.", WHITE)],
             [("She smiles slightly in response.", WHITE)],
             [("She ushers you to go into the maze with her.", WHITE)]]
 
     elif condition == "run_away":
         dialogue_lines = [
             [("Conflicted, you think back to this morning...", WHITE)],
-            [("You remember the bloody breakfast Mila made you.", WHITE)],
-            [("Looking at her, her cold, empty eyes, you make your decision:", WHITE)],
+            [("You remember the breakfast Mila made you.", WHITE)],
+            [("Her eyes are cold and empty...", WHITE)],
             [("Yep, not happening!", WHITE)],
-            [("You turn around and just book it!", WHITE)],
-            [("Run!!!", WHITE)]]
+            [("You turn around and just book it!", WHITE)]]
 
     current_line = 0
     dialogue_box = DialogueBox((40, 450, 720, 120))
@@ -49,10 +50,10 @@ def play_transition_s3(screen, clock, condition):
         # --- GUI --- #
         if condition == "follow_mila":
             screen.blit(maze, (0, 0))
-            screen.blit(pygame.transform.scale(mila_normal_dark, (195, 520)), (520, 120))
+            screen.blit(pygame.transform.scale(pygame.transform.flip(mila_normal_dark, True, False), (195, 520)), (520, 120))
 
             if current_line >= 1:
-                screen.blit(pygame.transform.scale(mila_happy_dark, (195, 520)), (520, 120))
+                screen.blit(pygame.transform.scale(pygame.transform.flip(mila_happy_dark, True, False), (195, 520)), (520, 120))
 
             if current_line >= 2:
                 screen.blit(maze, (0, 0))
@@ -60,14 +61,21 @@ def play_transition_s3(screen, clock, condition):
 
         elif condition == "run_away":
             screen.blit(maze, (0, 0))
-            screen.blit(pygame.transform.scale(mila_pensive_dark, (195, 520)), (520, 120))
+            screen.blit(pygame.transform.scale(pygame.transform.flip(mila_normal_dark, True, False), (195, 520)), (520, 120))
 
             if current_line >= 1:
-                screen.blit(pygame.transform.scale(mila_happy_dark, (195, 520)), (520, 120))
+                screen.blit(maze, (0, 0)) # replace with flashback
 
             if current_line >= 2:
                 screen.blit(maze, (0, 0))
-                screen.blit(pygame.transform.scale(mila_normal_dark, (90, 240)), (190, 210))
+                screen.blit(pygame.transform.scale(pygame.transform.flip(mila_pensive_dark, True, False), (195, 520)), (520, 120))
+
+            if current_line >= 3:
+                screen.blit(maze, (0, 0))
+                screen.blit(pygame.transform.scale(pygame.transform.flip(mila_normal_dark, True, False), (195, 520)), (520, 120))
+
+            if current_line >= 4:
+                screen.blit(pygame.transform.flip(maze, True, False), (0, 0))
 
         dialogue_box.update()
         dialogue_box.draw(screen)
