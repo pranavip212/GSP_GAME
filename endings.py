83b1f892894a841946c02650a843715aa30e0d19
chelseav87 <chelseav87@kprschools.ca>
2026-06-10 03:18:51 -0400

    modded endings

--- endings.py
+++ endings.py
@@ -1,19 +1,32 @@
 import pygame
+from constants import *
 
 def play_ending(screen, clock, choice, outcome):
-
-    # --- Endings --- #
-    if choice == "talk" and outcome == "good":
-        print("talk, good ending")
-    elif choice == "talk" and outcome == "bad":
-        print("talk, bad ending")
-
-    if choice == "fight" and outcome == "good":
-        print("fight, good ending")
-    elif choice == "fight" and outcome == "bad":
-        print("fight, bad ending")
+    main_font = pygame.freetype.SysFont("consolas", 70, bold=True)
+    sub_font = pygame.freetype.SysFont("consolas", 28, bold=False)
+    fade_alpha = 255
 
     # --- Main Game Loop --- #
     running = True
     while running:
         clock.tick(60)
+
+        if outcome == "good":
+            main_font.render_to(screen, (190, 100), "GOOD ENDING", WHITE)
+            if choice == "talk":
+                sub_font.render_to(screen, (290, 180), "You saved Mila.", WHITE)
+            elif choice == "talk":
+                pass
+
+        if choice == "fight" and outcome == "good":
+            pass
+        elif choice == "fight" and outcome == "bad":
+            pass
+
+        fade_surface = pygame.Surface((WIDTH, HEIGHT))
+        fade_surface.fill(BLACK)
+        fade_surface.set_alpha(fade_alpha)
+        screen.blit(fade_surface, (0, 0))
+        fade_alpha -= 2
+
+        pygame.display.flip()
